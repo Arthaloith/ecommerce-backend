29d6de9a116fc4a77d6e08de63c4d05a1f03da88
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-22 13:55:05 +0700

    config deployment

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -16,23 +16,33 @@ const sendEmailCreateOrder = async (email, orderItems) => {
   transporter.use('compile', inlineBase64({ cidPrefix: 'somePrefix_' }));
 
   let listItem = '';
-  const attachImage = []
+  const attachImage = [];
   orderItems.forEach((order) => {
-    listItem += `<div>
-    <div>
-      Bạn đã đặt sản phẩm <b>${order.name}</b> với số lượng: <b>${order.amount}</b> và giá là: <b>${order.price} VND</b></div>
-      <div>Bên dưới là hình ảnh của sản phẩm</div>
-    </div>`
-    attachImage.push({ path: order.image })
-  })
+    listItem += `
+      <div>
+        <div>
+          <p>Bạn đã đặt sản phẩm <b>${order.name}</b> với số lượng: <b>${order.amount}</b> và giá là: <b>${order.price} VND</b></p>
+          <p>Bên dưới là hình ảnh của sản phẩm:</p>
+        </div>
+      </div>
+      <div style="margin-bottom: 20px;">
+        <img src="${order.image}" alt="Product Image" style="max-width: 300px; height: auto;">
+      </div>
+    `;
+    attachImage.push({ path: order.image });
+  });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: process.env.MAIL_ACCOUNT, // sender address
     to: email, // list of receivers
-    subject: "Bạn đã đặt hàng tại shop LẬP trình thật dễ", // Subject line
-    text: "Hello world?", // plain text body
-    html: `<div><b>Bạn đã đặt hàng thành công tại shop Lập trình thật dễ</b></div> ${listItem}`,
+    subject: "You have placed an order at Amogus Store", // Subject line
+    html: `
+      <div style="font-family: Arial, sans-serif;">
+        <h2>Thanks for choosing our service</h2>
+        ${listItem}
+      </div>
+    `,
     attachments: attachImage,
   });
 }
